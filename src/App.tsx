936650db663c6aa42fdfbaefd6506c9b0f479c6c
Margot Paon de Andrade Garcia <margot@ciandt.com>
2021-04-20 10:17:19 -0300

    create validation to date and e-mail

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import * as yup from 'yup'
 import ChatBot from 'react-simple-chatbot'
 import { ThemeProvider } from 'styled-components'
 import ReactStars from 'react-rating-stars-component'
@@ -24,17 +23,6 @@ const theme = {
   userBubbleColor: '#fff',
   userFontColor: '#4a4a4a',
 }
-
-// validação dos inputs
-const validateSchema = yup.object().shape({
-  name: yup.string().required(),
-  email: yup.string().email().required(),
-  data: yup
-    .date()
-    .default(() => new Date())
-    .required(),
-  cidade: yup.string().required(),
-})
 
 // sistema da nota
 const ratingChanged = (newRating) => {
@@ -75,14 +63,84 @@ const steps = [
   {
     id: '6',
     user: true,
-    component: <input type="text" placeholder="00/00/0000" />,
+    validator: (value) => {
+      const regExNumbers = /[^\d]/ // somente numeros
+      const regExNoSpace = /^\s+|\s+$/g // retira espaço em branco
+
+      if (value.length !== 10) {
+        return 'data invalida, digite assim 12/09/1970'
+      }
+
+      const splitData = value.split('-')
+
+      if (splitData.length !== 3) {
+        return false
+      }
+
+      /* Retira os espaços em branco do início e fim de cada string. */
+      splitData[0] = splitData[0].replace(regExNoSpace, '')
+      splitData[1] = splitData[1].replace(regExNoSpace, '')
+      splitData[2] = splitData[2].replace(regExNoSpace, '')
+
+      if (splitData[0].length !== 2 || splitData[1].length !== 2 || splitData[2].length !== 4) {
+        return false
+      }
+
+      /* Procura por caracter não-numérico. EX.: o "x" em "28/09/2x11" */
+      if (
+        regExNumbers.test(splitData[0]) ||
+        regExNumbers.test(splitData[1]) ||
+        regExNumbers.test(splitData[2])
+      ) {
+        return false
+      }
+
+      const day = parseInt(splitData[0], 10)
+      const month = parseInt(splitData[1], 10) - 1 // O JavaScript representa o mês de 0 a 11 (0->janeiro, 1->fevereiro... 11->dezembro)
+      const year = parseInt(splitData[2], 10)
+      const newData = new Date(year, month, day)
+      const today = new Date()
+
+      if (newData > today) {
+        return false
+      }
+
+      if (
+        newData.getDate() !== day ||
+        newData.getMonth() !== month ||
+        newData.getFullYear() !== year
+      ) {
+        console.log('pass')
+      }
+      return true
+    },
     trigger: '7',
   },
   { id: '7', message: 'Agora me fala teu e-mail, por gentileza.', trigger: '8' },
   {
     id: '8',
     user: true,
-    component: <input type="text" placeholder="email@exemplo.com" />,
+    validator: (value) => {
+      const user = value.substring(0, value.indexOf('@'))
+      const domain = value.substring(value.indexOf('@') + 1, value.length)
+
+      if (
+        user.length >= 1 &&
+        domain.length >= 1 &&
+        user.search('@') === -1 &&
+        domain.search('@') === -1 &&
+        user.search(' ') === -1 &&
+        domain.search(' ') === -1 &&
+        domain.search('.') !== -1 &&
+        domain.indexOf('.') >= 1 &&
+        domain.lastIndexOf('.') < domain.length - 1
+      ) {
+        console.log('email valido')
+      } else {
+        return 'Digite um email válido nome@nome.com'
+      }
+      return true
+    },
     trigger: '9',
   },
   {
